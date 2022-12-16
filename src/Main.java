6f47a9ce080d5a2601cf7d6f1a2167362baa3bcb
buseong <77231519+buseong@users.noreply.github.com>
2022-12-17 03:51:27 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    static void pprint(String text) {
+        System.out.println(text);
+    }
+
+    public static void t_1(String[] args) {
+        String str = "ssss";
+
+        pprint(str);
+    }
+
+
+
+
+}
